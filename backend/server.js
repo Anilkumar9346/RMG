d8286395dcb11a5c0f01f9139b5f3419afbcac7f
Ankit Dalei <ankitdalei123@gmail.com>
2025-12-16 16:16:15 +0530

    test commit

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.use(notFound);
 app.use(errorHandler);
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
